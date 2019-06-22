ef96ffb942e212462b107d0ec93de0482a10b99f
pratyush1611 <51953929+pratyush1611@users.noreply.github.com>
2019-06-22 12:23:49 +0530

    made gdal work for python 3.7 finally it seems i was installing the wrong gdal version after all and pip wouldnt work to update numpy version so gdal wasnt working in 2.7
    added changes: getting unique values and counts from raster

--- value_getter.py
+++ value_getter.py
@@ -2,42 +2,47 @@
 import os
 import numpy as np
 from osgeo import gdal
-
+path = r"E:\earthEngine\million_plus\LULC\tester\try"
 def value_extractinator():
         raster_file =0
-
-        ds = gdal.Open(path + "/clipped_files/kota_1990_lulc_cnstn_clipped.tif")
+        ds = gdal.Open(path + "/cls_1990_nakli_lulc.tif")
         band =  ds.GetRasterBand(1)
         print(gdal.GetDataTypeName(band.DataType)   )
 
-        #array = np.array(band.ReadAsArray())
-        #values = np.unique(array)
-        # Projection
-        #print(ds.GetProjection())
+        array = np.array(band.ReadAsArray())
+        values = np.unique(array)
 
-        # Dimensions
-        #print(ds.RasterXSize)
-        #print(ds.RasterYSize)
-        # Read raster data as numeric array from GDAL Dataset
-        rasterArray = band  .ReadAsArray()
-        print(rasterArray)
-        # Number of bands
-        #print(ds.RasterCount)
-        #print("values"+ values)
+        rasterArray = band.ReadAsArray()
+        unique_array ,count_array = (np.unique(rasterArray,return_index=False, return_inverse=False, return_counts=True, axis=None))
+        array_unq_cnt = zip(unique_array,count_array)
+        for item in array_unq_cnt:
+            print(i[0] + " is " + i[1}+" times"])
         '''
         # Compute statistics if needed
         if band.GetMinimum() is None or band.GetMaximum()is None:
             band.ComputeStatistics(0)
             print("Statistics computed.")
 
-        # Fetch metadata for the band
-        band.GetMetadata()
 
         # Print only selected metadata:
         print ("[ NO DATA VALUE ] = ", band.GetNoDataValue()) # none
         print ("[ MIN ] = ", band.GetMinimum())
         print ("[ MAX ] = ", band.GetMaximum())
         '''
+        #////////////////////////////////
+        # Projection
+        #print(ds.GetProjection())
+        # Fetch metadata for the band
+        #print(band.GetMetadata())
+
+        # Dimensions
+        #print(ds.RasterXSize)
+        #print(ds.RasterYSize)
+        # Read raster data as numeric array from GDAL Dataset
+        # Number of bands
+        #print(ds.RasterCount)
+        #print( "values" + values)
+        #///////////////////////////////////
         #////////////////
         print("im done here")
 value_extractinator()
