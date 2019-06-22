31618fc3c2af9f8d51faf1b4561378e123aeeb6a
pratyush1611 <51953929+pratyush1611@users.noreply.github.com>
2019-06-22 14:51:45 +0530

    running on every file with a for loop and extracting their unique along with count of unique values in raster

--- value_getter.py
+++ value_getter.py
@@ -3,38 +3,42 @@ import os
 import numpy as np
 import pandas as pd
 from osgeo import gdal
-path = r"E:\earthEngine\million_plus\LULC\tester\try"
+path = r"E:\earthEngine\million_plus\LULC\newdata\clipped_files"
 def value_extractinator():
         raster_file =0
-        ds = gdal.Open(path + "/cls_1990_nakli_lulc.tif")
-        band =  ds.GetRasterBand(1)
-        print(gdal.GetDataTypeName(band.DataType)   )
+        #ds = gdal.Open(path + "/cls_1990_nakli_lulc.tif")
+        for file in os.listdir(path):
+            if (file.endswith(".tif")):
+                print(file)
+                ds = gdal.Open(path+ "/" +file)
+                band =  ds.GetRasterBand(1)
+                #print(gdal.GetDataTypeName(band.DataType)   )
+                rasterArray = band.ReadAsArray()
+                unique_array ,count_array = (np.unique(rasterArray,return_index=False, return_inverse=False, return_counts=True, axis=None))
+                array_unq_cnt = zip(unique_array,count_array)
+                for i in array_unq_cnt:
+                    print(i[0], i[1])
+                #data_years = pd.DataFrame(data=[], index=["nodata","urban","green","water","barren"], columns=[])
+                # Compute statistics if needed
+                if band.GetMinimum() is None or band.GetMaximum()is None:
+                    band.ComputeStatistics(0)
+                    print("Statistics computed.")
 
-        array = np.array(band.ReadAsArray())
-        values = np.unique(array)
 
-        rasterArray = band.ReadAsArray()
-        unique_array ,count_array = (np.unique(rasterArray,return_index=False, return_inverse=False, return_counts=True, axis=None))
-        array_unq_cnt = zip(unique_array,count_array)
-        data_years = pd.DataFrame(data=[], index=["nodata","urban","green","water","barren"], columns=[])
+                # Print only selected metadata:
+                print ("[ NO DATA VALUE ] = ", band.GetNoDataValue()) # none
+                print ("[ MIN ] = ", band.GetMinimum())
+                print ("[ MAX ] = ", band.GetMaximum())
 
-        c1992 = np.array([123,345,567,321])
+
+        '''
         data_years[1990] = count_array
         for i in array_unq_cnt:
             print(i[0], i[1])
         return(data_years)
         '''
-        # Compute statistics if needed
-        if band.GetMinimum() is None or band.GetMaximum()is None:
-            band.ComputeStatistics(0)
-            print("Statistics computed.")
 
 
-        # Print only selected metadata:
-        print ("[ NO DATA VALUE ] = ", band.GetNoDataValue()) # none
-        print ("[ MIN ] = ", band.GetMinimum())
-        print ("[ MAX ] = ", band.GetMaximum())
-        '''
         #////////////////////////////////
         # Projection
         #print(ds.GetProjection())
