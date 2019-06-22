1ae6f499e0462d11ce4648f472932f1998928331
pratyush1611 <51953929+pratyush1611@users.noreply.github.com>
2019-06-23 00:39:32 +0530

    asks user for path to set as workspace and acts accordingly

--- LULC_extracter.py
+++ LULC_extracter.py
@@ -1,4 +1,5 @@
 #!python2
+#above line torun from python 2 compiler
 #Description: to extract from lulc images to csv for furthur analyses
 
 # Import system modules
@@ -6,34 +7,16 @@ import os
 import arcpy
 from arcpy import env
 import numpy as np
-#from osgeo import gdal
 
-# Set workspace
-env.workspace = r"E:\earthEngine\million_plus\LULC\newdata"
-path = r"E:\earthEngine\million_plus\LULC\newdata"
-'''
-def read_folder(path):
-    lst_path = list()
-    file_name = list()
-    lst = os.listdir(path) # returns list
-    for i in lst:
-        if i.endswith(".tif"):
-            lst_path.append(path+ '\\' + i)
-            file_name.append(i)
-        else:
-            pass
-    return [lst_path, file_name]
-'''
 ##//////////clipper func
-
-def clipper_func():
+def clipper_func(clip_var = "kota_plng_bnd.shp"):
 
     for file in os.listdir(env.workspace):
         if(file.endswith(".tif")):
             print(file)
             #out_feature_class = r"E:\earthEngine\million_plus\LULC\tester\clipped"+file.strip(".tif")+"_clipped.tif"
             in_features = file
-            clip_features = "kota_plng_bnd.shp"
+            clip_features = clip_var
             out_feature_class = "/clipped_files/" + in_features[:-4]+"_clipped"
             arcpy.Clip_management(in_features,"#",out_feature_class + ".tif",
                                     clip_features,"NULL" ,"ClippingGeometry",
@@ -41,10 +24,17 @@ def clipper_func():
             print(file + "has been clipped")
 
     print("clipper has come to an end")
+####################################################
 
 
 
+#///////////////////////////////////////////////////
 print("code started")
+# Set workspace
+env.workspace = input("Enter the path to set as environment workspace : ")
+assert os.path.exists(path), "I did not find the file at, " + str(path)
+#env.workspace = r"E:\earthEngine\million_plus\LULC\newdata"
+clip_var = input("Enter the name of clipping feature that is the feature to act as boundary for clip : \n(please include the .shp at the end of the file name and ensure that it exists in the environment workspace) ")
+#run function to clip multiple
 clipper_func()
-
 print("code ended")
