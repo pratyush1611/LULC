50c007fa32767e676901162865e05e8a0ca369e6
Pratyush Kumar <51953929+pratyush1611@users.noreply.github.com>
2020-05-29 22:22:15 +0530

    NDinator updated to add NDBI functionality
    now added NDBI calculator, returns a list with 0 as NDVI and 1 as NDBI for given image

--- NDinator.js
+++ NDinator.js
@@ -2,12 +2,16 @@
 
 var NDinator = function(plngbnd , image)
 {
-  var ndvi;
+  var ndvi , ndbi;
   if (image.id().getInfo().toString() == "LC")
   {
     ndvi = image.normalizedDifference(['B5', 'B4']);
     ndbi = image.normalizedDifference(['B6', 'B5']);
   }
-
-  return ndvi;
+  else
+  {
+    ndvi = image.normalizedDifference(['B4', 'B3']);
+    ndbi = image.normalizedDifference(['B5', 'B4']);
+  }
+  return [ndvi , ndbi];
 };
